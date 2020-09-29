21cd1b468c52fc0f572cf9e2135ce8a27a4829cd
imd2121 <imaad.mufti@gmail.com>
2020-09-29 08:14:28 -0400

    First commit

---
+++ ToDoList/EntryViewController.swift
@@ -0,0 +1,58 @@
+//
+//  EntryViewController.swift
+//  ToDoList
+//
+//  Created by imaad mufti on 9/27/20.
+//
+
+import RealmSwift
+import UIKit
+
+class EntryViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var datePicker: UIDatePicker!
+    
+    private let realm = try! Realm()
+    public var completionHandler: (() -> Void)?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        textField.becomeFirstResponder()
+        textField.delegate = self
+        
+        datePicker.setDate(Date(), animated: true)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSaveButton))
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @IBAction func didTapSaveButton() {
+        if let text = textField.text, !text.isEmpty {
+            let date = datePicker.date
+            
+            realm.beginWrite()
+            
+            let newItem = ToDoListItem()
+            newItem.date = date
+            newItem.item = text
+            realm.add(newItem)
+            try! realm.commitWrite()
+            
+            completionHandler?()
+            navigationController?.popToRootViewController(animated: true)
+        }
+        else {
+            print("Add Something")
+        }
+    }
+    
+
+
+}
